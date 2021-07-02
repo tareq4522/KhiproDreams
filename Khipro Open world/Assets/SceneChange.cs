8e2d919f5aff3bfb531849a00b20c51875015c44
tareq4522 <refylclonegames71@gmail.com>
2021-07-02 20:49:46 +0600

    New scenes added
    scene choto kora hoise because of optimization , industrial pack added too

---
+++ Khipro Open world/Assets/SceneChange.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneChange : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void Quit()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
